7fb593843266f3809a56634be46f1fc6321bc750
李梦柯 （LI Mengke） <46624684+Keke921@users.noreply.github.com>
2024-07-01 10:56:14 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -84,3 +84,53 @@ class GCLLoss(nn.Module):
  
 
 
+class GCLAngLoss(nn.Module):
+    
+    def __init__(self, cls_num_list, m=0.5, epsilon = 0.1, weight=None, s=30, 
+                 easy_margin=False, train_cls=False, gamma=0.):
+        super(GCLAngLoss, self).__init__()
+        cls_list = torch.cuda.FloatTensor(cls_num_list)
+        m_list = torch.log(cls_list)
+        m_list = m_list.max()-m_list
+        self.m_list = m_list
+
+        assert s > 0
+        self.m = m
+        self.epsilon = epsilon
+        self.s = s
+        self.weight = weight
+        self.simpler = normal.Normal(0, 1/3)
+        self.easy_margin = easy_margin
+        self.train_cls = train_cls
+        self.gamma = gamma
+        
+    def forward(self, cosine, target):
+        index = torch.zeros_like(cosine, dtype=torch.uint8)
+        index.scatter_(1, target.data.view(-1, 1), 1)
+        
+        noise = self.simpler.sample(cosine.shape).clamp(-1, 1).to(cosine.device)
+        noise = math.pi/2*self.epsilon/self.m_list.max()*self.m_list*noise 
+        
+        noise_m = noise.abs()+self.m
+        m = torch.where(index, noise_m, noise.abs())   
+        
+        cos_m = torch.cos(m)
+        sin_m = torch.sin(m)
+        
+        th = torch.cos(math.pi - m)
+        mm = torch.sin(math.pi - m)
+        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
+        phi = cosine * cos_m - sine * sin_m
+        if self.easy_margin:
+            output = torch.where(cosine > 0, phi, cosine)               #theta<pi/2的地方，用theta+m替换; theta>pi/2用theta
+        else:
+        # this condition controls the theta+m should in range [0, pi]
+        #      0<=theta+m<=pi
+        #     -m<=theta<=pi-m
+        # when theta not in [0,pi], use cosface instead      
+            output = torch.where(cosine > th, phi, cosine - mm)         #phi中theta<pi-m的地方，是theta-m，theta > pi-m用cosface替换
+        if self.train_cls:
+            return focal_loss(F.cross_entropy(self.s*output, target, reduction='none', weight=self.weight), self.gamma)
+            #return focal_soft_label(self.s*output, target)
+        else:    
+            return F.cross_entropy(self.s*output, target, weight=self.weight) 
